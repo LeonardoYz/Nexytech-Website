d5dd5f065bde94e55398e70fa2f088b6393e823a
Leonardo Yzquierdo <leoyzquierdo@gmail.com>
2022-02-15 15:53:35 -0300

    create variables and header style

--- src/js/main.js
+++ src/js/main.js
@@ -1,1 +1,7 @@
-console.log("Hi")
+const headerNavigation = document.querySelector(".header__navigation")
+const mobileMenu = document.querySelector(".header__menu--mobile")
+
+mobileMenu.addEventListener("click", () => {
+  headerNavigation.classList.toggle("show")
+})
+
