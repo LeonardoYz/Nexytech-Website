5e360d8e1be5dbeb9b577a17269b32fbb7b6e49c
Leonardo Yzquierdo <leoyzquierdo@gmail.com>
2022-02-16 13:05:24 -0300

    added shadow in header when scrolling

--- src/js/main.js
+++ src/js/main.js
@@ -5,3 +5,17 @@ mobileMenu.addEventListener("click", () => {
   headerNavigation.classList.toggle("show")
 })
 
+const header = document.querySelector(".header")
+const headerHeight = header.offsetHeight
+
+function addShadowOnHeaderWhenScroll() {
+  if (window.scrollY >= headerHeight) {
+    header.classList.add("scroll")
+  } else {
+    header.classList.remove("scroll")
+  }
+}
+
+window.addEventListener("scroll", () => {
+  addShadowOnHeaderWhenScroll()
+})
