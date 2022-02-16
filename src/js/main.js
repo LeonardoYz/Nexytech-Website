daf344853fc3d63034fe8de438ffa2509fe2201b
Leonardo Yzquierdo <leoyzquierdo@gmail.com>
2022-02-16 17:05:22 -0300

    created preloading screen

--- src/js/main.js
+++ src/js/main.js
@@ -1,3 +1,19 @@
+const preloadingScreen = document.querySelector(".preloading")
+const preloadingProgressBar = document.querySelector(".preloading__fill")
+
+window.addEventListener("load", () => {
+  preloadingProgressBar.classList.add("loading-ending")
+
+  setTimeout(() => {
+    preloadingScreen.classList.add("hide")
+    preloadingProgressBar.classList.remove("loading-ending")
+
+    setTimeout(() => {
+      preloadingScreen.style.visibility = "hidden"
+    }, 500)
+  }, 1540)
+})
+
 const headerNavigation = document.querySelector(".header__navigation")
 const mobileMenu = document.querySelector(".header__menu--mobile")
 
